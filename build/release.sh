cd388503e1e4b2cb2af06a7b0e683df80fc97032
Stanislas Nanchen <stan@hood.ch>
2018-06-28 22:56:53 +0200

    Snapcraft pushing.

--- build/release.sh
+++ build/release.sh
@@ -5,5 +5,12 @@ if [ -z ${CIRCLE_TAG} ]; then
 	echo "No Tag = No Release"
 else
 	echo "Release: ${CIRCLE_TAG}"
-	curl -sL https://git.io/goreleaser | bash
+	openssl aes-256-cbc -d \
+              -in .circleci/credentials.enc \
+              -out credentials \
+              -k ${SNAP_CRED_PASSWORD}
+    snapcraft login --with credentials
+    rm credentials
+	goreleaser
+	find dist -name "skuld*.snap" | xargs snapcraft push
 fi
