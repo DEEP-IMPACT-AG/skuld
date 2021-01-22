29cd13b55545ab3f84c4d3af5565b4c70ebd3d73
vandr0iy <vandr0iy@pm.me>
2021-01-22 11:59:41 +0100

    removing snapcraft login

--- build/release.sh
+++ build/release.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-if [ -z ${CIRCLE_TAG} ]; then
+if [ -z "${CIRCLE_TAG}" ]; then
 	echo "No Tag = No Release"
 else
 	echo "Release: ${CIRCLE_TAG}"
-	openssl aes-256-cbc -d \
-              -in .circleci/credentials.enc \
-              -out credentials \
-              -k ${SNAP_CRED_PASSWORD}
-    snapcraft login --with credentials
-    rm credentials
+#	openssl aes-256-cbc -d \
+#              -in .circleci/credentials.enc \
+#              -out credentials \
+#              -k ${SNAP_CRED_PASSWORD}
+#    snapcraft login --with credentials
+#    rm credentials
 	goreleaser
-	find dist -name "skuld*.snap" | xargs snapcraft push --release edge
+#	find dist -name "skuld*.snap" | xargs snapcraft push --release edge
 fi
